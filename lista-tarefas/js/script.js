5323815b2081f37b20e9dd995bd0d5093439381b
AngelDevFront17 <angelica.silva1702@gmail.com>
2025-11-07 15:57:40 -0300

    Adicionando valores na lista de tarefas

--- lista-tarefas/js/script.js
+++ lista-tarefas/js/script.js
@@ -1,7 +1,14 @@
 // função em JavaScript
 function adicionar() {
+    //variáveis
     let tarefa = document.getElementById("tarefa");
+    let lista = document.getElementById("lista");
 
-    console.log(tarefa.value);
+    let item = document.createElement("li");
+    item.textContent = tarefa.value    
+
+    lista.appendChild(item);
     
+    tarefa.value = ""
+
 }
