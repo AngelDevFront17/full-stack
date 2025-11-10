cb600bcfd05292001d59215df8860f6f7316b589
AngelDevFront17 <angelica.silva1702@gmail.com>
2025-11-10 15:09:41 -0300

    adiocionando lógica

---
+++ logica-programacao/primeiro.js
@@ -0,0 +1,11 @@
+//Perguntando o nome para o usuário
+const userName = prompt("Qual é seu nome?")
+
+//perguntando para o usuário a idade
+const userAge = prompt("Qual é a sua idade?")
+
+//Imprimindo na tela o resultado
+document.write("Seu nome é: " + "<strong>" + userName + "</strong>" + "<br>")
+
+//Imprimindo a idade na tela
+document.write("Sua idade é: " + "<strong>" + userAge + "</strong>" + " anos")
---
+++ logica-programacao/segundo.js
@@ -0,0 +1,6 @@
+let numero1 = prompt("Digite um número")
+let numero2 = prompt("Digite outro número")
+
+
+
+console.log(numero1 + numero2)
