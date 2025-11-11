7f4139aa31cdc4ded68d5ac985af0ab9573876cf
AngelDevFront17 <angelica.silva1702@gmail.com>
2025-11-11 14:56:56 -0300

    Transformando anos em meses

--- logica-programacao/terceiro.js
+++ logica-programacao/terceiro.js
@@ -2,4 +2,12 @@
 let numero = Number (prompt("Digite um número"));
 let dobro = numero * 2;
 
-document.write("O dobro do número é: " , numero * 2);
+document.write("O dobro do número é: " + dobro);
+
+//Algoritmo: Converter horas em minutos. Início = Declare horas, minutos como números. Escreva("Digite o total de horas: "). Leia horas/ minutos ← horas * 60. Escreva("O total em minutos é: ", minutos). Fim
+
+let horas = Number (prompt ("Digite o total de horas: "))
+let minutos = horas * 60
+
+document.write("O total de minutos é: " + minutos);
+
