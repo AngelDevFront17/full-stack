6a3f23857ea869010ec497b0fdcbbbe5c8746234
AngelDevFront17 <angelica.silva1702@gmail.com>
2025-11-11 15:46:31 -0300

    Criando funções

---
+++ logica-programacao/funcao.js
@@ -0,0 +1,20 @@
+function resultAnimals(){
+    console.log("Animais");
+}
+// para executa a funcao
+resultAnimals();
+
+
+function timeFutebol(time){
+    document.write(time);
+}
+
+function pularLinha(){
+    document.write("<br>")
+}
+
+timeFutebol("Bangu");
+pularLinha();
+timeFutebol("Botafogo");
+pularLinha();
+timeFutebol("Palmeiras");
