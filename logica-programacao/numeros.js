d95dd0916ab878a393c7c1095f3ce640cff33fb0
AngelDevFront17 <angelica.silva1702@gmail.com>
2025-11-12 15:06:24 -0300

    treinando if

---
+++ logica-programacao/numeros.js
@@ -0,0 +1,11 @@
+/*Enunciado:
+Crie um programa que peça um número e mostre uma mensagem 
+dizendo se ele é positivo, negativo ou igual a zero.*/
+
+let numeros = Number (prompt("Digite um número: "))
+
+if(numeros > 0){
+    alert("Seu número é positivo")
+}else if(numeros <= 0){
+    alert("Seu número é negativo")
+}
