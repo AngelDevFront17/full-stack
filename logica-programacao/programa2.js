4cbd1b511c6c4b5d5c3556cefd8e241ef2935d8e
AngelDevFront17 <angelica.silva1702@gmail.com>
2025-11-12 13:55:22 -0300

    Descobrindo o dobro do valor com função

--- logica-programacao/programa2.js
+++ logica-programacao/programa2.js
@@ -1,3 +1,15 @@
+/*Início
+    Declare numero como número. 
+Função MostraDobro(valor)
+        Escreva("O dobro de ", valor, " é ", valor * 2)
+    FimFunção
+
+    Escreva("Digite um número: ")
+    Leia numero
+
+    Chame MostraDobro(numero)
+Fim*/
+
 let numero = Number (prompt("Digite um número:"))
 
 function mostrarValor(valor){
