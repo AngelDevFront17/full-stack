4ef92dca7a2381b9838050697b46648c1abccffe
AngelDevFront17 <angelica.silva1702@gmail.com>
2025-11-13 16:14:32 -0300

    Começando aprender laços de repetição com for

---
+++ logica-programacao/nomes.js
@@ -0,0 +1,6 @@
+const names = ["B2", "Rodrigo", "Victoria", "Dudu", "Alemão"];
+
+for(let i = 0; i < names.length; i++){
+    console.log("- " + names[i])
+}
+
---
+++ logica-programacao/numeros-for.js
@@ -0,0 +1,20 @@
+/*/*
+  Mostre na tela, um lista de números de 0 à 5
+  Exemplo: 
+  - Esse número é 0
+  - Esse número é 1
+  - Esse número é 2
+  - Esse número é 3
+  - Esse número é 4
+  - Esse número é 5
+*/
+
+const numeros = [0, 1, 2, 3, 4, 5]
+
+for(let i = 0; i < 6; i++){
+    console.log(numeros[i])
+}
+
+
+
+
