2e4ca0e9d62abf71cb18edc15e4603ab1bf41009
AngelDevFront17 <angelica.silva1702@gmail.com>
2025-11-14 15:04:55 -0300

    começando com while

--- logica-programacao/logica-4.js
+++ logica-programacao/logica-4.js
@@ -1,5 +1,5 @@
 const numbers = [90, 10 , 30];
-let sum = 0;
+let sum = 0; //utilizar um contador quando se tratar de soma, somar o contador mais a const[i]
 
 for (let i = 0; i < numbers.length; i++){
     sum = sum + numbers[i];
---
+++ logica-programacao/while.js
@@ -0,0 +1,6 @@
+let contador = 1;
+
+while(contador <= 5){
+    console.log(contador)
+    contador++; //diferente do for, o incremento vai embaixo (jamais esquecer o incremento)
+}
