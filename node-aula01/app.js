534fcf2f780d4fc657662b8c4bca13483dbda124
AngelDevFront17 <angelica.silva1702@gmail.com>
2025-11-24 14:56:54 -0300

    rodando servidor node.js

---
+++ node-aula01/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+//criar função
+//rota principal
+
+app.get('/', (req, res) => {
+    res.send('Bem vindo ao servidor Node.js')
+});
+
+//executando o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`)
+});
